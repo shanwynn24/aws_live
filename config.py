41c8207b2e504846c5d0b39c7636c210c2dfed26
shanwynn24 <69065457+shanwynn24@users.noreply.github.com>
2022-08-29 19:32:20 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ck9hrfiye8hq.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "teoshanwynn-employee"
+customregion = "us-east-1"
